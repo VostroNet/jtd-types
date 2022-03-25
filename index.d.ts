6ca5753df5ba289bc6b3b7e5961b770caf9d41f6
Matthew Mckenzie <mmckenzie@vostronet.com>
2022-03-25 12:27:27 +1000

    add unknown type and min type def

--- index.d.ts
+++ index.d.ts
@@ -22,6 +22,7 @@ export const enum JtdType {
   UINT16 = 'uint16',
   INT32 = 'int32',
   UINT32 = 'uint32',
+  UNKNOWN = "unknown"
 }
 
 export interface IJtdDict {
@@ -59,4 +60,68 @@ export interface IJtd {
 export interface IJtdMetadata {
   name: string,
   rootElement: boolean,
+}
+///--- min
+
+// https://github.com/smikhalevski/jtdc/blob/master/packages/types/src/main/jtd-types.ts
+/**
+ * JTD standard data types.
+ */
+ export const enum JtdMinType {
+  BOOLEAN = "b",
+  STRING = "st",
+
+  /**
+   * JSON strings containing an RFC3339 timestamp.
+   *
+   * ```ts
+   * "1985-04-12T23:20:50.52Z"
+   * ```
+   */
+  TIMESTAMP = "tz",
+  FLOAT32 = "f32",
+  FLOAT64 = "f64",
+  INT8 = "i8",
+  UINT8 = "ui8",
+  INT16 = "i16",
+  UINT16 = "ui16",
+  INT32 = "i32",
+  UINT32 = "ui32",
+  UNKNOWN = "u",
+}
+
+export interface IJtdMinDict {
+  [name: string]: IJtdMin;
+}
+
+/**
+ * The JTD with embedded definitions.
+ *
+ * @see https://tools.ietf.org/html/rfc8927 RFC8927
+ * @see https://jsontypedef.com/docs/jtd-in-5-minutes JTD in 5 minutes
+ */
+export interface IJtdMinRoot extends IJtdMin {
+  def?: IJtdMinDict; // definitions
+}
+
+/**
+ * The definition of a type.
+ */
+export interface IJtdMin {
+  md?: IJtdMinMetadata; // metadata
+  rq?: boolean; // opposite of nullable - required
+  ref?: string;
+  t?: JtdMinType | string; // type
+  em?: Array<string>; // enum
+  el?: IJtdMin; //elements
+  v?: IJtdMin; // values
+  p?: IJtdMinDict; // properties
+  d?: string; // discriminator
+  mp?: IJtdMinDict; // mapping
+  args?: IJtdMinDict; // arguments
+}
+
+export interface IJtdMinMetadata {
+  n: string; // name
+  re?: boolean; // rootElement
 }
